e6a8e633ea28bc592fab2e331120386c876ab0bf
Georges987 <ayenigeorgepierre@gmail.com>
2025-04-09 15:54:34 +0100

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-h1 text-center mt-16">
+    Scannez-moi !
+  </div>
+  <v-img
+    class="mt-10"
+    src="/code.png"
+    height="500"
+  ></v-img>
+</template>
+
+<script setup>
+  //
+</script>
